be867827e421e8443c5d163dfb011611f1a8981e
BohdanMoldavets <143205638+BohdanMoldavets@users.noreply.github.com>
2024-12-17 14:34:00 +0100

    Initial commit

---
+++ src/test/java/com/moldavets/SpringTelegramChannelManager/SpringTelegramChannelManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moldavets.SpringTelegramChannelManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTelegramChannelManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
